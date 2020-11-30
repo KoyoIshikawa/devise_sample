c0cba625573e6e082359ab7de4cb134b2d456516
KoyoIshikawa <koyoi561@gmail.com>
2020-11-30 12:16:32 +0900

    usersを利用したルーティングの設定（only: :showを利用）

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'items/index'
-  get 'items/new'
-  get 'items/create'
   root to:"items#index"
   devise_for :users
   resources :items
+
+  resource :users, only: :show
 end
